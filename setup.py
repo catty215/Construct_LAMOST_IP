ba883f2565031a0a3a0fdd0538b3b53e6a7758c2
catty215 <liucatty215@gmail.com>
2026-02-15 23:04:12 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='Construct_LAMOST_IP',
     version='0.1.0',
     description="LAMOST's IP recover using neural networks",
-    author='Qian Liu, Haotong Zhang',
+    author='Qian Liu',
     author_email='liuqian@bao.ac.cn',
     packages=find_packages(),
     include_package_data=True,
@@ -13,3 +13,4 @@ setup(
     ],
     license='MIT',
 )
+
